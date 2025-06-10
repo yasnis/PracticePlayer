7f64e559d153d3c768993e3418e1f3ed510aef1d
Yasuhiko Nishimura <nishimura-y@dentsu-crx.co.jp>
2025-06-10 16:45:48 +0900

    manifest

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,28 @@ export const metadata = {
   description: 'A player for practicing music',
   icons: {
     icon: '/practiceplayer/favicon.ico',
+    apple: [
+      { url: '/practiceplayer/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/practiceplayer/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
   },
+  manifest: '/practiceplayer/manifest.json',
+  appleWebApp: {
+    title: 'Practice Player',
+    statusBarStyle: 'black-translucent',
+    capable: true,
+    startupImage: [
+      { url: '/practiceplayer/icons/icon-512x512.png', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)' },
+    ],
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
+  themeColor: '#1E1E1E',
+  applicationName: 'Practice Player',
 };
 
 export default function RootLayout({ children }) {
